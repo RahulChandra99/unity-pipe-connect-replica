4b2c483622549eefca47e428a15881361920f625
Rahul Chandra <60950973+RahulChandra99@users.noreply.github.com>
2026-03-04 13:16:07 +0000

    level groups added

---
+++ Assets/Scripts/GamePlayPanel.cs
@@ -0,0 +1,62 @@
+﻿
+
+using System;
+using Game;
+using MyGame;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayPanel : ShowHidable
+{
+    [SerializeField] private Button _undoBtn;
+    [SerializeField] private Text _lvlTxt;
+
+    private void Start()
+    {
+        _lvlTxt.text = $"Level {LevelManager.Instance.Level.no}";
+    }
+
+    public void OnClickUndo()
+    {
+        LevelManager.Instance.OnClickUndo();
+    }
+
+    public void OnClickRestart()
+    {
+        GameManager.LoadGame(new LoadGameData
+        {
+            Level = LevelManager.Instance.Level,
+            LevelGroup = LevelManager.Instance.LevelGroup,
+        },false);
+    }
+
+    public void OnClickSkip()
+    {
+       
+
+        SharedUIManager.PopUpPanel.ShowAsConfirmation("Skip","Watch Video to skip this level", success =>
+        {
+            if(!success)
+                return;
+
+            
+          
+        });
+    }
+
+    public void OnClickMenu()
+    {
+        SharedUIManager.PopUpPanel.ShowAsConfirmation("Exit?","Are you sure want to exit the game?", success =>
+        {
+            if(!success)
+                return;
+
+            GameManager.LoadScene("MainMenu");
+        });
+    }
+
+    private void Update()
+    {
+        _undoBtn.interactable = LevelManager.Instance.HasUndo;
+    }
+}
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+using System.Collections;
+using MyGame;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        SharedUIManager.ConsentPanel.Show();
+        yield return new WaitUntil(() => !SharedUIManager.ConsentPanel.Showing);
+
+        GameManager.LoadScene("MainMenu");
+    }
+}
